9158bbbf68c4dd92f466d3728e5a644832186d0d
ERNEST CHRIS <ernestchris6@gmail.com>
2023-09-06 22:05:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Weather from './Component/WeatherApp/Weather';
+
+export default function App() {
+  return (
+    <div>
+        <Weather />
+    </div>
+  )
+}
