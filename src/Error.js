a5c899b271e57f179414f68f70492cbaf55958e2
ERNEST CHRIS <ernestchris6@gmail.com>
2024-07-29 15:09:20 +0100

    quiz

---
+++ src/Error.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Error() {
+  return (
+    <p className='error'>
+        <span>💥</span>
+        There was an error fetching questions.
+    </p>
+  )
+}
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='app-header'>
+        <img src='logo512.png' alt='react-logo' />
+        <h1>REACT QUIZ</h1>
+
+    </header>
+  )
+}
+
+export default Header;
