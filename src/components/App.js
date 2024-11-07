d5996b146a42345850c77ccee6d95c915b26c718
ERNEST CHRIS <ernestchris6@gmail.com>
2024-11-07 20:19:57 +0100

    json dep

--- src/components/App.js
+++ src/components/App.js
@@ -96,7 +96,7 @@ function App() {
  
 
   useEffect(function() {
-    fetch('http://localhost:8000/questions')
+    fetch('http://localhost:9000/questions')
     .then((res)=> res.json())
     .then((data)=> dispatch({type: "dataReceived", payload: data}))
     .catch((err)=> dispatch({type: "dataFailed"}))
